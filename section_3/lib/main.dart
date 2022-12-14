d929d6e0254665478cdfe8a28693b43f05d5135a
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-12-14 15:09:49 +0100

    Flutter training

--- section_3/lib/main.dart
+++ section_3/lib/main.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text(
-          "Test",
-          textAlign: TextAlign.center,
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          leading: const BackButton(),
+          centerTitle: true,
+          title: const Text(
+            "Test appbar",
+            style: TextStyle(color: Colors.red),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: const Center(
+          child: Text(
+            "Test body",
+            style: TextStyle(color: Colors.deepOrange),
+          ),
         ),
       ),
     ),
