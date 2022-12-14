a684e9ab22641e0c526cf359533c450739edb826
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-12-14 15:39:40 +0100

    Flutter training

--- section_3/lib/main.dart
+++ section_3/lib/main.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        leading: const BackButton(),
+        centerTitle: true,
+        title: const Text(
+          "Test appbar",
+          style: TextStyle(color: Colors.red),
+        ),
         backgroundColor: Colors.black,
-        appBar: AppBar(
-          leading: const BackButton(),
-          centerTitle: true,
-          title: const Text(
-            "Test appbar",
-            style: TextStyle(color: Colors.red),
+      ),
+      body: Column(
+        children: const <Widget>[
+          Center(
+            child: Text(
+              "Test body",
+              style: TextStyle(color: Colors.deepOrange),
+            ),
           ),
-          backgroundColor: Colors.black,
-        ),
-        body: const Center(
-          child: Text(
-            "Test body",
-            style: TextStyle(color: Colors.deepOrange),
-          ),
-        ),
+          Image(
+              image: NetworkImage(
+                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg')),
+        ],
       ),
     ),
-  );
+  ));
 }
