a77849733a3d40d5363615a1defa97ab7e04851c
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-12-21 13:14:10 +0100

    Layout training

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,12 +1,31 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black87,
-        body: Container(),
+  //Need to wrap it as a MaterialApp for the MediaQuery.of(context).size.height to work
+  runApp(const MaterialApp(home: W40K()));
+}
+
+class W40K extends StatelessWidget {
+  const W40K({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Container(
+          //Get the height of the screen and change it to 20%
+          height: MediaQuery.of(context).size.height * 0.2,
+          margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          alignment: Alignment.center,
+          color: Colors.red,
+          child: const Text(
+            "Test",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
