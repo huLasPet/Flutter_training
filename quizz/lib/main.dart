4f00cb8393c06cb109deb65765dd19ec57782f61
huLasPet <92375480+huLasPet@users.noreply.github.com>
2023-01-06 20:30:54 +0100

    Main functionality done, styling left

--- quizz/lib/main.dart
+++ quizz/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:html_unescape/html_unescape.dart';
 
 void main() => runApp(const Quizzler());
 
@@ -10,10 +14,10 @@ class Quizzler extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
-        body: const SafeArea(
+        body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(test: "String"),
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
           ),
         ),
       ),
@@ -22,81 +26,111 @@ class Quizzler extends StatelessWidget {
 }
 
 class QuizPage extends StatefulWidget {
-  const QuizPage({super.key, required String test});
+  final unescape = HtmlUnescape();
+  String question = "Tap any of the buttons to start";
+  String answer = "";
+  Color correct = Colors.blueGrey;
+  late String answerOption1 = "-";
+  late String answerOption2 = "-";
+  late String answerOption3 = "-";
+  late String answerOption4 = "-";
+  QuizPage({super.key});
 
   @override
   _QuizPageState createState() => _QuizPageState();
 }
 
 class _QuizPageState extends State<QuizPage> {
+  //Has to be here so it can use setState - otherwise the value of the button and what is displayed is different
+  void getQuestion(widget) async {
+    final rawData = await http
+        .get(Uri.parse('https://opentdb.com/api.php?amount=1&type=multiple'));
+    var responseData = json.decode(utf8.decode(rawData.bodyBytes));
+
+    //Create a list of all answers
+    List? answerList = [
+      widget.unescape.convert(responseData["results"][0]["correct_answer"])
+    ];
+    List? temp = responseData["results"][0]["incorrect_answers"];
+    answerList.addAll(temp!);
+
+    //Set buttons with possibly answers from the previous list
+    setState(
+      () {
+        widget.correct = Colors.blueGrey;
+        widget.question =
+            widget.unescape.convert(responseData["results"][0]["question"]);
+        widget.answer = widget.unescape
+            .convert(responseData["results"][0]["correct_answer"]);
+        widget.answerOption1 = widget.unescape
+            .convert(answerList.removeAt(Random().nextInt(answerList.length)));
+        widget.answerOption2 = widget.unescape
+            .convert(answerList.removeAt(Random().nextInt(answerList.length)));
+        widget.answerOption3 = widget.unescape
+            .convert(answerList.removeAt(Random().nextInt(answerList.length)));
+        widget.answerOption4 = widget.unescape
+            .convert(answerList.removeAt(Random().nextInt(answerList.length)));
+      },
+    );
+  }
+
+  Padding createButton(widgetNumber) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: widget.correct),
+        child: Text(
+          widgetNumber,
+          style: const TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () {
+          if (widget.answer == widgetNumber || widget.answer == "") {
+            setState(() {
+              widget.correct = Colors.green;
+              getQuestion(widget);
+            });
+          } else {
+            setState(() {
+              widget.correct = Colors.red;
+              getQuestion(widget);
+            });
+          }
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    final size = MediaQuery.of(context).size;
+    return ListView(
+      shrinkWrap: false,
       children: <Widget>[
-        const Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              widget.question,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
               ),
             ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.blueGrey,
-                  foregroundColor: Colors.white),
-              child: const Text(
-                'Answer 1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                //The user picked true.
-              },
+            SizedBox(
+              height: size.height * 0.20,
             ),
-          ),
+            createButton(widget.answerOption1),
+            createButton(widget.answerOption2),
+            createButton(widget.answerOption3),
+            createButton(widget.answerOption4),
+          ],
         ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.blueGrey),
-              child: const Text(
-                'Answer 2',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
-          ),
-        ),
-        //TODO: Add a Row here as your score keeper
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
