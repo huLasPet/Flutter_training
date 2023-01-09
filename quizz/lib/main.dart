907c0f4e9f9cf095b151016f8d8c120781ec3018
huLasPet <92375480+huLasPet@users.noreply.github.com>
2023-01-09 11:28:28 +0100

    Answer tracking
    Bottomsheet show the number of correct and incorrect answers

--- quizz/lib/main.dart
+++ quizz/lib/main.dart
@@ -34,6 +34,8 @@ class QuizPage extends StatefulWidget {
   late String answerOption2 = "-";
   late String answerOption3 = "-";
   late String answerOption4 = "-";
+  int correctNumber = 0;
+  int incorrectNumber = 0;
   QuizPage({super.key});
 
   @override
@@ -74,27 +76,62 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 
-  Padding createButton(widgetNumber) {
+  void settingModalBottomSheet(context) {
+    showModalBottomSheet(
+        context: context,
+        backgroundColor: Colors.grey.shade900,
+        builder: (BuildContext bc) {
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                    leading: const Icon(Icons.arrow_circle_up_outlined),
+                    iconColor: Colors.green,
+                    title: Text(
+                      'Correct - ${widget.correctNumber}',
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    onTap: () => {}),
+                ListTile(
+                    leading: const Icon(Icons.arrow_circle_down_outlined),
+                    iconColor: Colors.red,
+                    title: Text(
+                      'Incorrect - ${widget.incorrectNumber}',
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    onTap: () => {}),
+              ],
+            ),
+          );
+        });
+  }
+
+  Padding createButton(answerNumber) {
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: TextButton(
         style: TextButton.styleFrom(backgroundColor: widget.correctColor),
         child: Text(
-          widgetNumber,
+          answerNumber,
           style: const TextStyle(
             fontSize: 20.0,
             color: Colors.white,
           ),
         ),
         onPressed: () {
-          if (widget.answer == widgetNumber || widget.answer == "") {
+          if (widget.answer == answerNumber) {
             setState(() {
               widget.correctColor = Colors.green;
+              widget.correctNumber += 1;
               getQuestion(widget);
             });
+          } else if (widget.answer == "") {
+            getQuestion(widget);
           } else {
             setState(() {
               widget.correctColor = Colors.red;
+              widget.incorrectNumber += 1;
               getQuestion(widget);
             });
           }
@@ -109,6 +146,14 @@ class _QuizPageState extends State<QuizPage> {
     return ListView(
       reverse: true,
       children: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            settingModalBottomSheet(context);
+          },
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.grey.shade900, elevation: 0),
+          child: const Text('Stats'),
+        ),
         Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
