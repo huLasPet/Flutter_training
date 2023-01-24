6f9edc14bd9be5e9f762adf30d54161fb1b16866
huLasPet <92375480+huLasPet@users.noreply.github.com>
2023-01-24 16:47:28 +0100

    Added tests for existing methods

--- training_bloc/lib/cubit/counter_state.dart
+++ training_bloc/lib/cubit/counter_state.dart
@@ -1,7 +1,11 @@
 part of 'counter_cubit.dart';
 
-class CounterState {
+class CounterState extends Equatable {
   int counterValue;
   bool wasIncremented;
   CounterState({required this.counterValue, required this.wasIncremented});
+
+  @override
+  //Add the fields that are used with Equatable
+  List<Object?> get props => [counterValue, wasIncremented];
 }
---
+++ training_bloc/test/cubit/counter_cubit_test.dart
@@ -0,0 +1,38 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:test/test.dart';
+import 'package:training_bloc/cubit/counter_cubit.dart';
+
+void main() {
+  group('CounterCubit', () {
+    CounterCubit counterCubit = CounterCubit();
+
+    setUp(() {
+      counterCubit = CounterCubit();
+    });
+
+    tearDown(() {
+      counterCubit.close();
+    });
+
+    test(
+        'Testing the inital state, should return counterValue: 0 and wasIncremented: false',
+        () {
+      expect(counterCubit.state,
+          CounterState(counterValue: 0, wasIncremented: false));
+    });
+
+    blocTest(
+      'Testing increment, should return counterValue: 1 and wasIncremented: true',
+      build: () => counterCubit,
+      act: (cubit) => cubit.increment(),
+      expect: () => [CounterState(counterValue: 1, wasIncremented: true)],
+    );
+
+    blocTest(
+      'Testing decrement, should return counterValue: -1 and wasIncremented: false',
+      build: () => counterCubit,
+      act: (cubit) => cubit.decrement(),
+      expect: () => [CounterState(counterValue: -1, wasIncremented: false)],
+    );
+  });
+}
