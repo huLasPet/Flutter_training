67fd3898b2b8090bfb603565e99d9c2c6a2b86e7
huLasPet <92375480+huLasPet@users.noreply.github.com>
2023-01-30 10:25:27 +0100

    Bloc training end

--- training_bloc/lib/logic/cubit/counter_cubit.dart
+++ training_bloc/lib/logic/cubit/counter_cubit.dart
@@ -13,7 +13,7 @@ class CounterCubit extends Cubit<CounterState> with HydratedMixin {
   void decrement() => emit(CounterState(
       counterValue: state.counterValue - 1, wasIncremented: false));
 
-  //These 2 overrides are created automatically from the error, just need to change what is returned
+  //These 2 overrides are created automatically from the error after adding HydratedMixin, just need to change what is returned
   @override
   CounterState? fromJson(Map<String, dynamic> json) {
     return CounterState.fromMap(json);
